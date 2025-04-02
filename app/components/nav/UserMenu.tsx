37cf65f1acb2b7fc0e53835f72dd7e63df00bb62
Evelinarudz <95478655+Evelinarudz@users.noreply.github.com>
2025-04-02 19:17:10 +0300

    Add Login and Sign Up forms

---
+++ app/components/nav/UserMenu.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useCallback, useState } from "react";
+import Avatar from "../Avatar";
+import { AiFillCaretDown } from "react-icons/ai";
+import Link from "next/link";
+import MenuItem from "./MenuItem";
+import { signOut } from "next-auth/react";
+import BackDrop from "./BackDrop";
+
+const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toogleOpen = useCallback(() => {
+        setIsOpen(prev => !prev)
+    }, [])
+
+    return (
+    <>
+    <div className="relative z-30">
+        <div onClick={toogleOpen} className="p-2 border-[1px] border-slate-400 flex
+        flex-row items-center gap-1 rounded-full cursor-pointer hover:shadow-md transition text-slate-700">
+            <Avatar/>
+            <AiFillCaretDown/>
+        </div>
+        {isOpen && (
+            <div className="absolute rounded-md shadow-md w-[170px] bg-white overflow-hidden right-0 top-12
+            text-sm flex flex-col cursor-pointer">
+                <div>
+                    <Link href="/orders">
+                    <MenuItem onClick={toogleOpen}>Your Orders</MenuItem></Link>
+                    <Link href="/admin">
+                    <MenuItem onClick={toogleOpen}>Admin Dashboard</MenuItem></Link>
+                    <MenuItem onClick={() => {
+                        toogleOpen()
+                        signOut()
+                    }}>
+                    Logout
+                    </MenuItem>
+                </div>
+                <div>
+                <Link href="/login">
+                <MenuItem onClick={toogleOpen}>Login</MenuItem></Link>
+                <Link href="/register">
+                    <MenuItem onClick={toogleOpen}>Sign Up</MenuItem></Link>
+                </div>
+            </div>
+        )}
+    </div>
+    {isOpen ? <BackDrop onClick={toogleOpen}/> : null}
+    </>
+    );
+}
+ 
+export default UserMenu;
---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { useState } from "react";
+import Heading from "../components/Heading";
+import Input from "../components/inputs/input";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../components/Button";
+import Link from "next/link";
+import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios";
+import {signIn} from 'next-auth/react'
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+const RegisterForm = () => {
+
+    const [isLoading, setIsLoading] = useState(false)
+    const {register, handleSubmit, formState:{errors}} = useForm<FieldValues>({
+        defaultValues:{
+            name: "",
+            email: "",
+            password: "",
+        }
+    })
+
+    const router = useRouter()
+
+    const onSubmit:SubmitHandler<FieldValues> = (data) =>
+    {
+        setIsLoading(true)
+        axios.post('/api/register', data).then(() => {
+            toast.success("Account created!")
+
+            signIn("credentials", {
+                email: data.email,
+                password: data.password,
+                redirect: false,
+            }).then((callback) => {
+                if(callback?.ok){
+                    router.push('/cart');
+                    router.refresh();
+                    toast.success('Logged in!');
+                }
+
+                if(callback?.error){
+                    toast.error(callback.error)
+                }
+            })
+        }).catch(() => toast.error("Somthing went wrong")).finally(() => {
+            setIsLoading(false)
+        })
+    }
+
+    return ( <>
+    <Heading title="Sign up for E~Shop"/>
+    <Button outline label="Sign up with Google" icon={AiOutlineGoogle} onClick={() => {}} />
+    <hr className="bg-slate-300 w-full h-px"/>
+    <Input
+    id="name"
+    label="Name"
+    disabled={isLoading}
+    register={register}
+    errors={errors}
+    required
+    />
+    <Input
+    id="email"
+    label="Email"
+    disabled={isLoading}
+    register={register}
+    errors={errors}
+    required
+    />
+    <Input
+    id="password"
+    label="Password"
+    disabled={isLoading}
+    register={register}
+    errors={errors}
+    required
+    type="password"
+    />
+    <Button label={isLoading ? "Loading" : 'Sign Up'} onClick={handleSubmit(onSubmit)}/>
+    <p className="text-sm">
+        Already have an account? <Link className="underline" href='/login'>
+        Log in 
+        </Link>
+    </p>
+    </> );
+}
+ 
+export default RegisterForm;
