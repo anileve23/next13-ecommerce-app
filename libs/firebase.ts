5ae1a513064dd152051dbb0778ba95db58447db0
Evelinarudz <95478655+Evelinarudz@users.noreply.github.com>
2025-05-13 19:08:12 +0300

    Add product form

---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3tjOE7qS3JT_Le4EGSwYx2yk66h6f_mI",
+  authDomain: "e-shop-4ffc4.firebaseapp.com",
+  projectId: "e-shop-4ffc4",
+  storageBucket: "e-shop-4ffc4.firebasestorage.app",
+  messagingSenderId: "92526052471",
+  appId: "1:92526052471:web:ee1b821d4465c1f5d7f272"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp
