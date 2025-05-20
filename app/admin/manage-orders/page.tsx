165825c9961425216a31a8a7cfded6b8d6e3c2fc
Evelinarudz <95478655+Evelinarudz@users.noreply.github.com>
2025-05-20 21:46:32 +0300

    Add manage products and orders table to admin dashboard

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,6 +1,22 @@
-const ManageOrders = () => {
-    return ( <div className="text-slate-800">
-        Manage Oders
+import ManageOrdersClient from "./ManageOrdersClient";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import Container from "@/app/components/Container";
+import NullData from "@/app/components/NullData";
+import getOrders from "@/actions/getOrders";
+
+const ManageOrders = async () => {
+
+    const orders = await getOrders()
+    const currentUser = await getCurrentUser()
+
+    if(!currentUser || currentUser.role !== 'ADMIN'){
+        return <NullData title="Oops! Access denied :("/>
+    }
+
+    return ( <div className="pt-8">
+        <Container>
+        <ManageOrdersClient orders = {orders}/>
+        </Container>
     </div> );
 }
  
