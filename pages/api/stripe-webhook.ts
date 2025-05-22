3646284df9c008089e62df0d24eae03552c01c0c
Evelinarudz <95478655+Evelinarudz@users.noreply.github.com>
2025-05-22 23:34:34 +0300

    Add prismadb

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 import { buffer } from "micro";
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prismadb"
 
 export const config = {
     api: {
