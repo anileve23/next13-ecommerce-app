6a920d287bb65be95fcfe1e7bfc17acc8527a275
Evelinarudz <95478655+Evelinarudz@users.noreply.github.com>
2025-05-22 21:46:18 +0300

    Fix errors

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -1,18 +1,22 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
-import { NextResponse } from "next/server";
-import prisma from "@/libs/prismadb"
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/libs/prismadb";
 
-export async function DELETE(request: Request, {params}: {params: {id: string}}){
-    const currentUser = await getCurrentUser();
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const currentUser = await getCurrentUser();
 
-    if(!currentUser) return NextResponse.error();
+  if (!currentUser) return NextResponse.error();
 
-    if(currentUser.role !== "ADMIN"){
-        return NextResponse.error();
-    }
+  if (currentUser.role !== "ADMIN") {
+    return NextResponse.error();
+  }
 
-    const product = await prisma?.product.delete({
-        where: {id: params.id}
-    })
-    return NextResponse.json(product)
+  const product = await prisma?.product.delete({
+    where: { id: params.id },
+  });
+
+  return NextResponse.json(product);
 }
